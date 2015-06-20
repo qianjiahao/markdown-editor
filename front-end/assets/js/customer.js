ba27c2562f2323a568decac2a38fe25192c7c864
william <1041897388@qq.com>
2015-06-20 14:22:27 +0800

    modify the move function

--- front-end/assets/js/customer.js
+++ front-end/assets/js/customer.js
@@ -1,18 +1,23 @@
 var timer;
 
 function updateViewHeight() {
-	var scrollHeight = document.getElementById('outputText').scrollHeight;
-	var scrollTop = document.getElementById('outputText').scrollTop;
-	var offsetHeight = document.getElementById('outputText').offsetHeight;
+	var outputText = document.getElementById('outputText');
+	var scrollHeight = outputText.scrollHeight;
+	var scrollTop = outputText.scrollTop;
+	var offsetHeight = outputText.offsetHeight;
 
+
+	// if customer move the window more than 1/10 offsetHeight , the auto update view will disabled .
 	var isUpdate = scrollTop + offsetHeight / 10 > scrollHeight - offsetHeight ;
 
+	// set time to delay the window move .
 	if(isUpdate) {
 		if(timer) {
 			clearTimeout(timer);
 		}
 		timer = setTimeout(function () {
 			outputText.scrollTop = outputText.scrollHeight;
-		},350);
+			console.log(outputText);
+		},550);
 	}
 }
